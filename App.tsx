8942ab6cb5a2b6def22e840515d9be4e588cbf44
LexiSux <72486618+LexiSux@users.noreply.github.com>
2025-10-29 04:40:52 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
+import { QueryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@components/ui/toaster";
+import TooltipProvider from "@components/ui/tooltip";
 import { AuthProvider } from "@/lib/auth";
 import HomePage from "@/pages/HomePage";
 import BrowsePage from "@/pages/BrowsePage";
 import CommunityPage from "@/pages/CommunityPage";
 import PricingPage from "@/pages/PricingPage";
-import NotFound from "@/pages/not-found";
+import NotFound from "@/pages/NotFound";
 
-function Router() {
+function AppRouter() {
   return (
     <Switch>
       <Route path="/" component={HomePage} />
@@ -28,7 +28,7 @@ function App() {
       <AuthProvider>
         <TooltipProvider>
           <Toaster />
-          <Router />
+          <AppRouter />
         </TooltipProvider>
       </AuthProvider>
     </QueryClientProvider>
